e9b5509e649e5db5066973c729e383930b445f9c
Braulio  Resendiz Cordova <br19777@srcs.loc>
2017-03-01 09:29:28 -0800

    user story 1

--- UC12_functions.js
+++ UC12_functions.js
@@ -1,2 +1,34 @@
 
 
+function getAreaCode(phoneNum) {
+
+    var areaCode;
+
+    try {
+        areaCode = between(phoneNum, "(", ")");
+        areaCode = areaCode.trim();
+        if (areaCode.length == 3 && Number(areaCode)) {
+            return areaCode;
+        } else {
+            throw new Error("Invalid area code: " + areaCode);
+        }
+    } catch (error) {
+        throw new Error("Invalid phone number: " + error.message);
+    }
+}
+
+function displayAreaCode(inputId, outputId) {
+    var outputText = "";
+    var phoneNum = document.getElementById(inputId).value;
+
+    // Now try to get the code
+    try {
+        var areaCode = getAreaCode(phoneNum);
+        outputText = "Your area code is " + areaCode;
+    } catch (error) {
+        console.log(error.message);
+        outputText = error.message;
+    }
+
+    document.getElementById(outputId).innerHTML = outputText;
+}
