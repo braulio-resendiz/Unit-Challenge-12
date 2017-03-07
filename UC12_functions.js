f321d8fb79945818e1188edeaa520e4319d54c1b
Ethan S Pletcher <ep64523@srcs.loc>
2017-03-07 08:25:20 -0800

    Fixed js error display

--- UC12_functions.js
+++ UC12_functions.js
@@ -15,7 +15,7 @@ function between(string, start, end) {
           startAt += start.length;
           var endAt = string.indexOf(end, startAt);
           if (end == undefined) {
-                    endAt = string.length - 1;
+                    endAt = string.length;
           }
           if (endAt == -1) {
                     throw new Error("No end found: " + end);
@@ -27,77 +27,12 @@ function between(string, start, end) {
  * @param   {string} phoneNum The phone number
  * @returns {string} The area code
  */
-function getCOCode(phoneNum) {
-          var COCode;
-          try {
-                    COCode = between(phoneNum, " ", "-");
-          }
-          catch (error) {
-                    COCode = undefined;
-          }
-          return COCode
-}
-
-function getLineCode(phoneNum) {
-          var LineCode;
-          try {
-                    LineCode = between(phoneNum, "-");
-          }
-          catch (error) {
-                    LineCode = undefined;
-          }
-          return LineCode
-}
-
 function displayOutput(a, b) {
           displayAreaCode(a, b);
           displayCOCode(a, b);
           displayLineCode(a, b);
 }
 
-function displayAreaCode(inputId, outputId) {
-          document.getElementById(outputId).innerHTML = "";
-          var input = "'" + document.getElementById(inputId).value + "'";
-          var outputText = "";
-          if (getAreaCode(input) != undefined) {
-                    outputText = "The Area Code is " + getAreaCode(input) + ".";
-                    document.getElementById(outputId).style.color = "blue";
-          }
-          else if (getAreaCode(input) == undefined) {
-                    outputText = "Error";
-                    document.getElementById(outputId).style.color = "blue";
-          }
-          document.getElementById(outputId).innerHTML = outputText;
-}
-
-function displayCOCode(inputId, outputId) {
-          var input = "'" + document.getElementById(inputId).value + "'";
-          var outputText = "";
-          if (getCOCode(input) != undefined) {
-                    outputText = "The CO Code is " + getCOCode(input) + ".";
-                    document.getElementById(outputId).style.color = "blue";
-          }
-          else if (getCOCode(input) == undefined) {
-                    outputText = "Error";
-                    document.getElementById(outputId).style.color = "blue";
-          }
-          document.getElementById(outputId).innerHTML += "</br>" + outputText;
-}
-
-function displayLineCode(inputId, outputId) {
-          var input = "'" + document.getElementById(inputId).value + "'";
-          var outputText = "";
-          if (getLineCode(input) != undefined) {
-                    outputText = "The Line Code is " + getLineCode(input) + ".";
-                    document.getElementById(outputId).style.color = "blue";
-          }
-          else if (getLineCode(input) == undefined) {
-                    outputText = "Error";
-                    document.getElementById(outputId).style.color = "blue";
-          }
-          document.getElementById(outputId).innerHTML += "</br>" + outputText;
-}
-
 function getAreaCode(phoneNum) {
           var areaCode;
           try {
@@ -119,12 +54,12 @@ function getCOCode(phoneNum) {
           var COCode;
           try {
                     COCode = between(phoneNum, " ", "-");
-                    areaCode = areaCode.trim();
+                    COCode = COCode.trim();
                     if (COCode.length == 3 && Number(COCode)) {
-                              return areaCode;
+                              return COCode;
                     }
                     else {
-                              throw new Error("Invalid CO code: " + areaCode);
+                              throw new Error("Invalid CO code: " + COCode);
                     }
           }
           catch (error) {
@@ -132,16 +67,16 @@ function getCOCode(phoneNum) {
           }
 }
 
-function getAreaCode(phoneNum) {
-          var areaCode;
+function getLineCode(phoneNum) {
+          var lineCode;
           try {
-                    areaCode = between(phoneNum, "(", ")");
-                    areaCode = areaCode.trim();
-                    if (areaCode.length == 3 && Number(areaCode)) {
-                              return areaCode;
+                    lineCode = between(phoneNum, "-");
+                    lineCode = lineCode.trim();
+                    if (lineCode.length == 4 && Number(lineCode)) {
+                              return lineCode;
                     }
                     else {
-                              throw new Error("Invalid area code: " + areaCode);
+                              throw new Error("Invalid line code: " + lineCode);
                     }
           }
           catch (error) {
@@ -156,6 +91,36 @@ function displayAreaCode(inputId, outputId) {
           try {
                     var areaCode = getAreaCode(phoneNum);
                     outputText = "Your area code is " + areaCode;
+          }
+          catch (error) {
+                    console.log(error.message);
+                    outputText = error.message;
+          }
+          document.getElementById(outputId).innerHTML += "</br>" + outputText;
+}
+
+function displayCOCode(inputId, outputId) {
+          var outputText = "";
+          var phoneNum = document.getElementById(inputId).value;
+          // Now try to get the code
+          try {
+                    var COCode = getCOCode(phoneNum);
+                    outputText = "Your CO code is " + COCode;
+          }
+          catch (error) {
+                    console.log(error.message);
+                    outputText = error.message;
+          }
+          document.getElementById(outputId).innerHTML += "</br>" + outputText;
+}
+
+function displayLineCode(inputId, outputId) {
+          var outputText = "";
+          var phoneNum = document.getElementById(inputId).value;
+          // Now try to get the code
+          try {
+                    var lineCode = getLineCode(phoneNum);
+                    outputText = "Your line code is " + lineCode;
           }
           catch (error) {
                     console.log(error.message);
