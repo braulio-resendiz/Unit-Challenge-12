9cfeb1ae353e5d23dd9cb2d6c5f7cb7902b653d3
Ethan S Pletcher <ep64523@srcs.loc>
2017-03-09 08:52:02 -0800

    Test finish

--- test_12.js
+++ test_12.js
@@ -5,48 +5,60 @@ QUnit.test("Test the between function.", function (assert) {
           var result = between(str1, "/");
           assert.deepEqual(result, "0243gj", "We expect result to be 0243gj");
 });
-QUnit.test("Test the displayOutput function.", function (assert) {
-          document.getElementById('input').innerHTML = "456";
-          document.getElementById('output').innerHTML = "456";
-          displayOutput('input', 'output');
-          assert.equal(document.getElementById("output").innerHTML, "", "Passed - Expected ");
-});
 QUnit.test("Test the getAreaCode function.", function (assert) {
-          var str1 = '(415) 457-7512';
+          var str1 = '(415) 555-7512';
           var result = getAreaCode(str1);
           assert.deepEqual(result, "415", "We expect result to be 415");
 });
 QUnit.test("Test the getCOCode function.", function (assert) {
-          var str1 = '(415) 457-7512';
+          var str1 = '(415) 555-7512';
           var result = getCOCode(str1);
-          assert.deepEqual(result, "457", "We expect result to be 457");
+          assert.deepEqual(result, "555", "We expect result to be 555");
 });
 QUnit.test("Test the getLineCode function.", function (assert) {
-          var str1 = '(415) 457-7512';
+          var str1 = '(415) 555-7512';
           var result = getLineCode(str1);
           assert.deepEqual(result, "7512", "We expect result to be 7512");
 });
-QUnit.test("Test the displayAreaCode function.", function (assert) {
-          addDigit("3");
-          squareButton();
-          assert.equal(currentInputdisp, "(3)^2", "Passed - (3)^2");
-          assert.equal(currentInputreal, "square(3)", "Passed - square(3)");
-          currentInputdisp = '';
-          currentInputreal = '';
+QUnit.test( "Errors thrown for getAreaCode", function( assert ) {
+    assert.throws( function() {
+        getAreaCode("415) 555-7512");
+    }, "Missing '('. An error should have been thrown." );
+
+    assert.throws( function() {
+        getAreaCode("(415 555-7512");
+    }, "Missing ')'. An error should have been thrown." );
+    assert.throws( function() {
+        getAreaCode("(41555) 555-7512");
+    }, "Invalid area code: 41555. An error should have been thrown." );
+    assert.throws( function() {
+        getAreaCode("(4j5) 555-7512");
+    }, "Invalid area code: 4j5. An error should have been thrown." );
 });
-QUnit.test("Test the displayCOCode function.", function (assert) {
-          addDigit("3");
-          squareButton();
-          assert.equal(currentInputdisp, "(3)^2", "Passed - (3)^2");
-          assert.equal(currentInputreal, "square(3)", "Passed - square(3)");
-          currentInputdisp = '';
-          currentInputreal = '';
+QUnit.test( "Errors thrown for getCOCode", function( assert ) {
+    assert.throws( function() {
+        getCOCode("(415)555-7512");
+    }, "Missing ' '. An error should have been thrown." );
+
+    assert.throws( function() {
+        getCOCode("(415) 5557512");
+    }, "Missing '-'. An error should have been thrown." );
+    assert.throws( function() {
+        getCOCode("(415) 55-7512");
+    }, "Invalid CO code: 55. An error should have been thrown." );
+    assert.throws( function() {
+        getCOCode("(415) 5g5-7512");
+    }, "Invalid CO code: 5g5. An error should have been thrown." );
 });
-QUnit.test("Test the displayLineCode function.", function (assert) {
-          addDigit("3");
-          squareButton();
-          assert.equal(currentInputdisp, "(3)^2", "Passed - (3)^2");
-          assert.equal(currentInputreal, "square(3)", "Passed - square(3)");
-          currentInputdisp = '';
-          currentInputreal = '';
+QUnit.test( "Errors thrown for getLineCode", function( assert ) {
+    assert.throws( function() {
+        getLineCode("(415) 5557512");
+    }, "Missing '-'. An error should have been thrown." );
+    assert.throws( function() {
+        getLineCode("(415) 555-71512");
+    }, "Invalid line code: 71512. An error should have been thrown." );
+    assert.throws( function() {
+        getLineCode("(415) 555-7r12");
+    }, "Invalid line code: 7r12. An error should have been thrown." );
+
 });
